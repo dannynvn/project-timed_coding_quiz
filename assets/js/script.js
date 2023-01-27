b878b2d211b584c0ee018bbb367d941316366060
Danny Nguyen <dannynvn@gmail.com>
2023-01-26 16:03:40 -0800

    added code to reset options and question functionality works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var startButton = document.querySelector(".start-button");
 var userScore = document.querySelector(".score");
 var timerEl = document.querySelector(".timer");
+var questionNumber = 1;
 
 var questionTitle = document.querySelector(".question-text");
 var questionOptionOne = document.querySelector("#option-button-one");
@@ -38,9 +39,22 @@ var questionThree = {
 //start quiz when user clicks start button
 function startQuiz() {
     //variable to check if quiz should continue
-    var quizComplete = false
+    var quizComplete = false;
+    showQuestionOne();
+
 }
 
+//function to reset event listeners
+function resetOptions() {
+    questionOptionOne.removeEventListener("click",correctAnswer);
+    questionOptionTwo.removeEventListener("click",correctAnswer);
+    questionOptionThree.removeEventListener("click",correctAnswer);
+    questionOptionFour.removeEventListener("click",correctAnswer);
+    questionOptionOne.removeEventListener("click",wrongAnswer);
+    questionOptionTwo.removeEventListener("click",wrongAnswer);
+    questionOptionThree.removeEventListener("click",wrongAnswer);
+    questionOptionFour.removeEventListener("click",wrongAnswer);
+}
 
 //show first question
 
@@ -50,7 +64,38 @@ function startQuiz() {
 
 //show third question
 
+function correctAnswer() {
+    resultText.textContent = "Correct!";
+    userScore.textContent++
+    questionNumber++
+    console.log("correct answer is chosen");
+    if (questionNumber == 2) {
+        resetOptions();
+        showQuestionTwo();
+    } else if (questionNumber == 3) {
+        resetOptions()
+        showQuestionThree();
+    } else {
+        console.log("game over");
+    }
+}
 
+function wrongAnswer() {
+    resultText.textContent = "Incorrect!";
+    console.log("incorrect answer is chosen");
+    questionNumber++
+    //subtract 10 second from timer
+    //move onto next question
+    if (questionNumber == 2) {
+        resetOptions();
+        showQuestionTwo();
+    } else if (questionNumber == 3) {
+        resetOptions()
+        showQuestionThree();
+    } else {
+        console.log("game over");
+    }
+}
 //function to check if answer is correct
 //add point to user score
 
@@ -62,7 +107,7 @@ function startQuiz() {
 //check if quiz should end
 function checkQuizStatus() {
     //if all questions answered or timer is 0, then quiz ends
-    quizComplete = true
+    quizComplete = true;
 }
 
 //setScore function
@@ -83,102 +128,58 @@ function checkQuizStatus() {
 
     // showQuestionOne();
 
-    // function correctAnswer() {
-    //     resultText.textContent = "Correct!";
-    //     userScore.textContent++
-    //     questionNumber++
-    //     console.log("correct answer is chosen");
-    //     if (questionNumber == 2) {
-    //         questionOptionOne.removeEventListener("click",correctAnswer);
-    //         questionOptionTwo.removeEventListener("click",correctAnswer);
-    //         // questionOptionThree.removeEventListener("click",correctAnswer);
-    //         // questionOptionFour.removeEventListener("click",correctAnswer);
-    //         showQuestionTwo();
-    //     } else if (questionNumber == 3) {
-    //         questionOptionOne.removeEventListener("click",correctAnswer);
-    //         // questionOptionTwo.removeEventListener("click",correctAnswer);
-    //         questionOptionThree.removeEventListener("click",correctAnswer);
-    //         // questionOptionFour.removeEventListener("click",correctAnswer);
-    //         showQuestionThree();
-    //     } else {
-    //         console.log("game over");
-    //     }
-    //     //move onto next question
-    // }
+
+
+//questions are visible for user when this function is called
+function showQuestionOne() {
+    questionTitle.textContent = questionOne.question;
+    questionOptionOne.textContent = questionOne.options[0];
+    questionOptionTwo.textContent = questionOne.options[1];
+    questionOptionThree.textContent = questionOne.options[2];
+    questionOptionFour.textContent = questionOne.options[3];
+
+    //event listeners checking for user input
+    questionOptionOne.addEventListener("click",correctAnswer, {once : true});
+    questionOptionTwo.addEventListener("click", wrongAnswer, {once: true});
+    questionOptionThree.addEventListener("click", wrongAnswer,  {once: true});
+    questionOptionFour.addEventListener("click", wrongAnswer, {once: true});
     
-    // function wrongAnswer() {
-    //     resultText.textContent = "Incorrect!";
-    //     console.log("incorrect answer is chosen");
-    //     questionNumber++
-    //     //subtract 10 second from timer
-    //     //move onto next question
-    //     if (questionNumber == 2) {
-    //         questionOptionOne.removeEventListener("click",correctAnswer);
-    //         questionOptionTwo.removeEventListener("click",correctAnswer);
-    //         // questionOptionThree.removeEventListener("click",correctAnswer);
-    //         // questionOptionFour.removeEventListener("click",correctAnswer);
-    //         showQuestionTwo();
-    //     } else if (questionNumber == 3) {
-    //         questionOptionOne.removeEventListener("click",correctAnswer);
-    //         // questionOptionTwo.removeEventListener("click",correctAnswer);
-    //         questionOptionThree.removeEventListener("click",correctAnswer);
-    //         // questionOptionFour.removeEventListener("click",correctAnswer);
-    //         showQuestionThree();
-    //     } else {
-    //         console.log("game over");
-    //     }
-    // }
-
-    // //questions are visible for user when this function is called
-    // function showQuestionOne() {
-    //     questionTitle.textContent = questionOne.question;
-    //     questionOptionOne.textContent = questionOne.options[0];
-    //     questionOptionTwo.textContent = questionOne.options[1];
-    //     questionOptionThree.textContent = questionOne.options[2];
-    //     questionOptionFour.textContent = questionOne.options[3];
-
-    //     //event listeners checking for user input
-    //     questionOptionOne.addEventListener("click",correctAnswer, {once : true});
-    //     questionOptionTwo.addEventListener("click", wrongAnswer, {once: true});
-    //     questionOptionThree.addEventListener("click", wrongAnswer,  {once: true});
-    //     questionOptionFour.addEventListener("click", wrongAnswer, {once: true});
-        
-    //     return;
-    // }
-
-    // function showQuestionTwo() {
-    //     questionTitle.textContent = questionTwo.question;
-    //     questionOptionOne.textContent = questionTwo.options[0];
-    //     questionOptionTwo.textContent = questionTwo.options[1];
-    //     questionOptionThree.textContent = questionTwo.options[2];
-    //     questionOptionFour.textContent = questionTwo.options[3];
-
-    //     //event listeners checking for user input
-    //     questionOptionOne.addEventListener("click",wrongAnswer, {once : true});
-    //     questionOptionTwo.addEventListener("click", wrongAnswer, {once : true});
-    //     questionOptionThree.addEventListener("click", correctAnswer, {once : true});
-    //     questionOptionFour.addEventListener("click", wrongAnswer, {once : true});
-
-    //     return;
-    // }
-
-    // function showQuestionThree() {
-    //     questionTitle.textContent = questionThree.question;
-    //     questionOptionOne.textContent = questionThree.options[0];
-    //     questionOptionTwo.textContent = questionThree.options[1];
-    //     questionOptionThree.textContent = questionThree.options[2];
-    //     questionOptionFour.textContent = questionThree.options[3];
-
-    //     //event listeners checking for user input
-    //     questionOptionOne.addEventListener("click",wrongAnswer, {once : true});
-    //     questionOptionTwo.addEventListener("click", correctAnswer, {once : true});
-    //     questionOptionThree.addEventListener("click", wrongAnswer, {once : true});
-    //     questionOptionFour.addEventListener("click", wrongAnswer, {once : true});
-
-    //     return;
-    // }
-
+    return;
 }
+
+function showQuestionTwo() {
+    questionTitle.textContent = questionTwo.question;
+    questionOptionOne.textContent = questionTwo.options[0];
+    questionOptionTwo.textContent = questionTwo.options[1];
+    questionOptionThree.textContent = questionTwo.options[2];
+    questionOptionFour.textContent = questionTwo.options[3];
+
+    //event listeners checking for user input
+    questionOptionOne.addEventListener("click",wrongAnswer, {once : true});
+    questionOptionTwo.addEventListener("click", wrongAnswer, {once : true});
+    questionOptionThree.addEventListener("click", correctAnswer, {once : true});
+    questionOptionFour.addEventListener("click", wrongAnswer, {once : true});
+
+    return;
+}
+
+function showQuestionThree() {
+    questionTitle.textContent = questionThree.question;
+    questionOptionOne.textContent = questionThree.options[0];
+    questionOptionTwo.textContent = questionThree.options[1];
+    questionOptionThree.textContent = questionThree.options[2];
+    questionOptionFour.textContent = questionThree.options[3];
+
+    //event listeners checking for user input
+    questionOptionOne.addEventListener("click",wrongAnswer, {once : true});
+    questionOptionTwo.addEventListener("click", correctAnswer, {once : true});
+    questionOptionThree.addEventListener("click", wrongAnswer, {once : true});
+    questionOptionFour.addEventListener("click", wrongAnswer, {once : true});
+
+    return;
+}
+
+
 
 
 
